dffe2fccb40ef4732c6934459f6e0beab1d1c0a7
HuangPengyuan <929957466h@gmail.com>
2023-08-14 00:30:20 +0800

    spring-cache

---
+++ springcache-demo/springcachedemo.sql
@@ -0,0 +1,12 @@
+
+create database spring_cache_demo;
+
+use spring_cache_demo;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
